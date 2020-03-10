79edc3a670f79bfb3a22049af4c55084c9297525
Morgan-Leblanc <morgan.leblanc91@gmail.com>
2020-03-10 11:59:51 +0100

    end of blogpost

---
+++ src/context/BlogContext.js
@@ -0,0 +1,59 @@
+import createDataContext from "./createDataContext";
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "edit_blogpost":
+      return state.map(blogPost => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
+    case "add_blogpost":
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 9999),
+          title: action.payload.title,
+          content: action.payload.content
+        }
+      ];
+    case "delete_blogpost":
+      return state.filter(blogPost => blogPost.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+const deleteBlogPost = dispatch => {
+  return id => {
+    dispatch({ type: "delete_blogpost", payload: id });
+  };
+};
+const addBlogPost = dispatch => {
+  return (title, content, callback) => {
+    dispatch({ type: "add_blogpost", payload: { title, content } });
+    if (callback) {
+      callback();
+    }
+  };
+};
+
+const editBlogPost = dispatch => {
+  return (id, title, content, callback) => {
+    dispatch({
+      type: "edit_blogpost",
+      payload: { id, title, content }
+    });
+    if (callback) {
+      callback();
+    }
+  };
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  {
+    addBlogPost,
+    deleteBlogPost,
+    editBlogPost
+  },
+  [{ title: "TestPost", content: "TestContent", id: 1 }]
+);
